bcef8b1ad721c5f11282c7b6da44ed288b56f5e9
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-09 17:13:27 -0300

    adding new phrase

--- magic8ball.py
+++ magic8ball.py
@@ -21,7 +21,7 @@ answer = ''
 
 
 
-random_number = random.randint(1, 9)
+random_number = random.randint(1, 10)
 
 
 if random_number == 1:
@@ -35,13 +35,15 @@ elif random_number == 4:
 elif random_number == 5:
   answer = '“Just as happiness does not last forever, neither does sadness".'
 elif random_number == 6:
-  answer = '“Pain is not eternal, if you allow it, your best is enough”.'
+  answer = '“Pain is not eternal, it will pass”.'
 elif random_number == 7:
   answer = '“You did not come this far by chance for everything there is a reason. Keep going”.'
 elif random_number == 8:
   answer = '“Allow yourself to start and start over as many times as necessary.”.'
 elif random_number == 9:
   answer = '“Your competition is with yourself.”.'
+elif random_number == 10:
+  answer = '"Your best is enough"'
 else:
   answer = "Error"
 
