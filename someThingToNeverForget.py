8263b8a914fdb78d00f13faea04032184ac74ac3
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-21 20:24:13 -0300

    Trust yourself, baby

---
+++ someThingToNeverForget.py
@@ -0,0 +1,6 @@
+name = input("Hello, darling, tell me your name, please?")
+
+if name == '':
+  print('We wanna tell you that we believe in you and you should never be afraid of being yourself')
+else:
+  print('Well, sweetie', name,'! And we believe in you and you should never be afraid of being yourself')
