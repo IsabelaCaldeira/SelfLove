f11881b54cd8c247f54f7b054152f55fe7358161
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-21 20:30:02 -0300

    the new taylor is on right now

---
+++ swifttie.py
@@ -0,0 +1,7 @@
+swiftie = input("Are you Taylor Swift?")
+
+if swiftie == 'n':
+  print('I got confused, cause you are as gorgeous as her')
+else:
+  print('Well, now all this gorgeous soul of yours make sense')
+
