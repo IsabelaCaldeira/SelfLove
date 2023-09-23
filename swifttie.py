9fdda77b5181808739f9c1727d8f52cd0939c241
Isabela Caldeira Cruz <30991310+IsabelaCaldeira@users.noreply.github.com>
2023-09-23 23:23:33 +0200

    Actualizar swifttie.py

--- swifttie.py
+++ swifttie.py
@@ -1,6 +1,6 @@
 swiftie = input("Are you Taylor Swift?")
 
-if swiftie == 'n' or swiftie == "Não":
+if swiftie == "N" or swiftie == "Não":
   print('I got confused, cause you are as gorgeous as her')
 else:
   print('Well, now all this gorgeous soul of yours make sense')
